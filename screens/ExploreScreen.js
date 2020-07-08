a925b41f14ba0649bcfedaec4c92c6204567d553
jobran41 <jobran41@gmail.com>
2020-07-08 01:11:02 +0100

    fix charts

--- screens/ExploreScreen.js
+++ screens/ExploreScreen.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React, {Component} from 'react';
+import {View, Text, Button, StyleSheet} from 'react-native';
 
-const ExploreScreen = () => {
+import {WebView} from 'react-native-webview';
+import PureChart from 'react-native-pure-chart';
+//import Echarts from 'native-echarts';
+
+class ExploreScreen extends Component {
+  render() {
+    let sampleData = [
+      {
+        seriesName: 'series1',
+        data: [
+          {x: '2018-02-01', y: 30},
+          {x: '2018-02-02', y: 200},
+          {x: '2018-02-03', y: 170},
+          {x: '2018-02-04', y: 250},
+          {x: '2018-02-05', y: 10},
+        ],
+        color: '#297AB1',
+      },
+      /*       {
+        seriesName: 'series2',
+        data: [
+          {x: '2018-02-01', y: 20},
+          {x: '2018-02-02', y: 100},
+          {x: '2018-02-03', y: 140},
+          {x: '2018-02-04', y: 550},
+          {x: '2018-02-05', y: 40},
+        ],
+        color: 'yellow',
+      }, */
+    ];
     return (
-      <View style={styles.container}>
-        <Text>ExploreScreen</Text>
-        <Button
-          title="Click Here"
-          onPress={() => alert('Button Clicked!')}
-        />
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <PureChart width={'100%'} height={300} data={sampleData} type="line" />
       </View>
     );
-};
+  }
+}
 
 export default ExploreScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
-    alignItems: 'center', 
-    justifyContent: 'center'
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
