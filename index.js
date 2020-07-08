ddb602a4af8c0b3bd4b767ff2d98b5b2d22a606a
jobran41 <jobran41@gmail.com>
2020-07-08 02:07:45 +0100

    add redux

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 /**
  * @format
  */
+import React from 'react';
 import 'react-native-gesture-handler';
+
 import {AppRegistry} from 'react-native';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+
+import reducers from './modules';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const store = createStore(reducers);
+
+const RNRedux = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => RNRedux);
---
+++ modules/user.reducer.js
@@ -0,0 +1,13 @@
+const INITIAL_STATE = {
+  user: {},
+  possible: ['Allie', 'Gator', 'Lizzie', 'Reptar'],
+};
+
+const users = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default users;
